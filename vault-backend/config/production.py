cda30e1972bffe3c56fd2ce6f61b31a251eb66d1
Vikrant Pogula <vikrant.pogula@gmail.com>
2014-08-14 13:53:36 +0530

    feat(config): temporarily removed email configs

--- vault-backend/config/production.py
+++ vault-backend/config/production.py
@@ -76,12 +76,12 @@ class Production(Common):
 
     # Email
     DEFAULT_FROM_EMAIL = values.Value('Vault <vikrant.pogula@gmail.com>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
+    # EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    # EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    # EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    # EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    # EMAIL_SUBJECT_PREFIX = values.Value('[] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    # EMAIL_USE_TLS = True
     SERVER_EMAIL = DEFAULT_FROM_EMAIL
     # END EMAIL
 
